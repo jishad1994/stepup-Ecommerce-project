06ec0a885a0022883f1e3470e51c4fe48c0a9320
jishad kolapurath <jishadkolapurath@gmail.com>
2025-02-09 22:54:33 +0530

    added the static middle ware for uploads

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/uploads", express.static("public/uploads"));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(nocache());
